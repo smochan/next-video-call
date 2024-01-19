917e405f03f4bc9f71d79e76d110840c83ddb893
mochan <img_2019052@iiitm.ac.in>
2024-01-19 18:34:54 +0530

    final global chat

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,12 +1,13 @@
 'use client'
 
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, use } from 'react'
 import * as Ably from 'ably';
-import { AblyProvider, useChannel} from 'ably/react';
+import { AblyProvider, useChannel, usePresence} from 'ably/react';
 import usePeer from '../utils/usePeer';
 
 import styles from '@@/styles/Room.module.css'
 import { v4 as uuidv4 } from "uuid";
+import handleUserLeft from '@@/utils/handleUserLeft';
 
 const ChatRoom = () =>{
 
@@ -19,14 +20,15 @@ const ChatRoom = () =>{
 
   return(
     <AblyProvider client={client}>
-      <ChatRoomClient userId={userId}/>
+      <ChatRoomClient room={'global-chat'} userId={userId}/>
     </AblyProvider>
   )
 }
 
-const ChatRoomClient = (props: {userId: string}) => {
+const ChatRoomClient = (props: {room: string, userId: string}) => {
   const userId = props.userId;
-  const [streams, setStreams] = useState<Array<{stream: MediaStream, peerId: string}>>([]);
+  const room = props.room;
+  const [streams, setStreams] = useState<Array<{stream: MediaStream, peerId: string, userId: string, mic: boolean, camera: boolean}>>([]);
   const [chats, setChats] = useState<Array<{message: string, name: string, id: string}>>([]);
   const [name, setName] = useState<{saved: boolean, name: string}>({saved: false, name: ""});
 
@@ -35,40 +37,66 @@ const ChatRoomClient = (props: {userId: string}) => {
 
   const { peer, myPeerId, isPeerReady } = usePeer();
 
-  const { channel } = useChannel('global-chat', (data) => {
+  const { channel } = useChannel(room, (data) => {
     console.log("new message received: ", data.data, " clientId: ", data.clientId, "stream length: ", streams.length);
     const action = data.data.action;
-      if(action === 'send-message') setChats(prev => [...prev, {message: data.data.message, name: data.data.name, id: data.clientId}])
+      if(action === 'new-message') setChats(prev => [...prev, {message: data.data.message, name: data.data.name, id: data.clientId}])
       if(action === 'join-room' && isPeerReady && data.data.peerId!== myPeerId) {
-        const call = peer.call(data.data.peerId, streams[0]?.stream, {metadata: {peerId: myPeerId}});
+        const call = peer.call(data.data.peerId, streams[0]?.stream, {metadata: {peerId: myPeerId, userId: userId}});
+        console.log("calling.....")
+        let num = true;
         call.on("stream", (userVideoStream: MediaStream) => {
-          console.log("making call.....")
-          setStreams(prev => [...prev, {stream: userVideoStream, peerId: data.data.peerId}]);
+          console.log("setting call after calling.....")
+          const streamData = {stream: userVideoStream, peerId: data.data.peerId, userId: data.clientId, mic: true, camera: true}
+          if(num) setStreams(prev => [...prev, streamData]);
+          num = false;
         })
       }
-  })
-
-
-
-  const handleSelfStream = (device: string) => {
-      const videoElement = document.getElementById(userId) as HTMLVideoElement;
-      if (videoElement) {
-        if(device === "mic") {
-          videoElement.muted = !videoElement.muted;
-          return;
+      if(action === 'mic' ) {
+        const videoElement = document.getElementById(data.data.peerId) as HTMLVideoElement;
+        if (videoElement && data.data.peerId !== myPeerId) {
+          videoElement.muted = data.data.status;
         }
-        if(device === "camera") {
-          if(videoElement.srcObject == null){
-            videoElement.srcObject = streams[0].stream;
+      }
+      if(action === 'cam' ) {
+        const videoElement = document.getElementById(data.data.peerId) as HTMLVideoElement;
+        if (videoElement && data.data.peerId !== myPeerId) {
+          if(data.data.status){
             videoElement.play();
           }
           else{
-            videoElement.srcObject = null;
+            videoElement.pause();
           }
+        }
+      }
+  })
+
+  const { presenceData } = usePresence(room, (data: any) => {
+    console.log("presence callBack data", data);
+  })
+
+  console.log("presenceData: ", presenceData, presenceData.length, "streams: ", streams);
+
+  if(streams && streams.length && (presenceData.length < streams.length)) {
+    const arr = handleUserLeft(streams, presenceData, userId);
+    console.log("arr: ", arr);
+    setStreams(arr);
+  }
+
+  const handleSelfStream = (device: string) => {
+      const videoElement = document.getElementById(myPeerId) as HTMLVideoElement;
+      if (videoElement) {
+        if(device === "mic") {
+          channel.publish(room, {action: "mic", peerId: myPeerId, status: !videoElement.muted})
+          // videoElement.muted = !videoElement.muted;
+          return;
+        }
+        if(device === "camera") {
+            channel.publish(room, {action: "cam", peerId: myPeerId, status: videoElement.paused ? true : false})
+            videoElement.paused ? videoElement.play() : videoElement.pause();
         }
     }
   }
-
 
   useEffect(() => {
     const getOwnVideo = async () => {
@@ -76,12 +104,12 @@ const ChatRoomClient = (props: {userId: string}) => {
         audio: true,
         video: true,
       });
-      setStreams([{stream: stream, peerId: myPeerId}]);
+      setStreams([{stream: stream, peerId: myPeerId, userId: userId, mic: true, camera: true}]);
     }
 
     if(isPeerReady && myPeerId !== '') {
       getOwnVideo();
-      channel.publish('global-chat', { action: "join-room", name: name.name, peerId: myPeerId });
+      channel.publish(room, { action: "join-room", name: name.name, peerId: myPeerId, userId: userId });
     }
 
   }, [isPeerReady, myPeerId]);
@@ -93,14 +121,16 @@ const ChatRoomClient = (props: {userId: string}) => {
       peer.on("call", (call: any) => {
         console.log("getting calll.......", streams.length);
         call.answer(streams[0]?.stream);
+        let num = true;
         
         call.on("stream", (userVideoStream: MediaStream) => {
           console.log("setting call")
           // handleStreams(userVideoStream, call.metadata.userId);
-          setStreams(prev => [...prev, {stream: userVideoStream, peerId: call.metadata.peerId}]);
+          const streamData = {stream: userVideoStream, peerId: call.metadata.peerId, userId: call.metadata.userId ,mic: true, camera: true}
+          if( num) setStreams(prev => [...prev, streamData]);
+          num = false;
         })
       })
-      // console.log("inside if");
     }
 }
 
@@ -115,31 +145,29 @@ const ChatRoomClient = (props: {userId: string}) => {
         <div className={styles.main__left}>
           <div className={styles.videos__group}>
             <div className={styles.video_grid}>
-              { streams.length ? streams.map((stream, index) => (
-                index%2 == 0 ? <video  id={stream.peerId} key={index} className={styles.video} muted autoPlay ref={(videoRef) => {
-                  if (videoRef) {
-                    videoRef.srcObject = stream.stream;
-                  }
-                }} /> : ""
-              )) : <p>no stream</p>
+              { streams.length && streams.map((stream, index) => (
+                <video  id={stream.peerId} key={index} className={styles.video} autoPlay ref={(videoRef) => { if (videoRef) videoRef.srcObject = stream.stream }} />
+                ))
               }
             </div>
           </div>
           <div className={styles.options}>
             <div className={styles.options__left}>
-              <div className={styles.options__button} onClick={() => handleSelfStream("camera")}>
+              <div className={styles.options__button} onClick={(e) => handleSelfStream("camera")}>
                 <i className="fa fa-video-camera" aria-hidden="true">
                   cam
                 </i>
               </div>
-              <div className={styles.options__button } onClick={() => handleSelfStream("mic")}>
+              <div className={styles.options__button } onClick={(e) => {
+                console.log(e.target);
+                handleSelfStream("mic")
+              }}>
                 {/* <i className="fa fa-microphone" aria-hidden="true"> */}
                   mic
                 {/* </i> */}
               </div>
             </div>
             <div className={styles.options__right}>
-
               {name.saved ? 
               <p> Hey, {name.name}</p>
               : 
@@ -178,7 +206,7 @@ const ChatRoomClient = (props: {userId: string}) => {
               // onChange={(e) => setMsg(e.target.value)}
               ref={msgRef}
             />
-            { channel !== undefined && <div className={styles.options__button} onClick={() => msgRef.current?.value && channel.publish('global-chat', {message: msgRef.current.value, action: "send-message", name: name.saved ? name.name : "No-name"})}>
+            { channel !== undefined && <div className={styles.options__button} onClick={() => msgRef.current?.value && channel.publish(room, {message: msgRef.current.value, action: "new-message", name: name.saved ? name.name : "No-name"})}>
               <i className="fa fa-plus" aria-hidden="true">
                 send
               </i>
